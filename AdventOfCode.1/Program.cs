8367408a2ddd9d7b621a4e819b3fc7574d840309
Radek Rous <radek.rous@riganti.cz>
2018-12-07 14:14:46 +0100

    Day 1

---
+++ AdventOfCode.1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AdventOfCode._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var partResults = new List<int>();
+            string s;
+            var sum = 0;
+            while (true)
+            {
+                using (var streamReader = new StreamReader("input.txt"))
+                {
+                    while (!string.IsNullOrEmpty(s = streamReader.ReadLine()))
+                    {
+                        sum += Convert.ToInt32(s);
+                        if (partResults.Contains(sum))
+                        {
+                            Console.WriteLine($"First repeating frequency is: {sum}");
+                            return;
+                        }
+                        partResults.Add(sum);
+                    }
+                    Console.WriteLine($"Result frequency is: {sum}");
+                }
+
+            }
+        }
+    }
+}
